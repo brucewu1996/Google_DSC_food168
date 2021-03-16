a6b1ea71226239b1a4abe0243f3e9dc254aaa383
iamy8000 <amy8000@gmail.com>
2021-03-16 21:11:56 +0800

    add calendar function, add HomeActivity

---
+++ app/src/main/java/com/example/androidcamera/HomeActivity.kt
@@ -0,0 +1,44 @@
+package com.example.androidcamera
+
+import android.app.DatePickerDialog
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_home.*
+import java.util.*
+
+class HomeActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        btnDatePicker.setOnClickListener { view ->
+            clickDatePicker(view)
+        }
+    }
+
+    private fun clickDatePicker(view: View) {
+        val myCalender = Calendar.getInstance()
+        val year = myCalender.get(Calendar.YEAR)
+        val month = myCalender.get(Calendar.MONTH)
+        val day = myCalender.get(Calendar.DAY_OF_MONTH)
+
+        val dpd = DatePickerDialog(
+            this,
+            DatePickerDialog.OnDateSetListener { View, selectedYear, selectedMonth, selectedDayofMonth ->
+                //pop-up window after selection
+                Toast.makeText(
+                    applicationContext,
+                    "You picked $selectedMonth $selectedDayofMonth, $selectedYear ",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }, year, month, day
+        )
+        dpd.datePicker.setMaxDate(Date().time)
+        dpd.show()
+
+    }
+
+}
